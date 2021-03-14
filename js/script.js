43c32c50624024998a2bcc50a354bf9a7775a895
Angela Purcell <kalmpurcell@gmail.com>
2021-03-14 20:19:04 +0000

    testing out flip cards in about me

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@
     x.style.display = "block";
   }
 }
+
+
+/* flip the cards in about me*/
+
+$(".flipper").click(function() {
+  var target = $( event.target );
+  if ( target.is("a") ) {
+    //follow that link
+    return true;
+  } else {
+    $(this).toggleClass("flip");
+  }
+  return false;
+});
